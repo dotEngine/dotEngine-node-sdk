e4bb149e49b74e3318a9fdd1f9920dcacafc3c0f
notedit <notedit@gmail.com>
2017-05-14 16:18:16 +0800

    fix parmas

--- dotengine.js
+++ dotengine.js
@@ -8,6 +8,8 @@ dotEngine server SDK
 
 const net = require('net');
 const request = require('request');
+const jwt = require('jwt-simple');
+
 
 const dotEngineUrl = 'https://janus.dot.cc/api/';
 const tokenExpires = 3600*24;
@@ -40,21 +42,29 @@ class DotEngine
             expires = options.expires;
         }
 
+        let data = {
+            room:room,
+            user:user,
+            appkey:this._appkey,
+            expires:expires,
+            role:role 
+        };
+
+        let sign = jwt.encode(data,this._appSecret);
+
         let roptions = {
             uri: dotEngineUrl + 'createToken',
             method:'POST',
             json: {
-                room:room,
-                user:user,
                 appkey:this._appkey,
-                expires:expires,
-                role:role
+                sign:sign 
             }
         };
 
         request(roptions, (error,response,body) => {
             if(!error && response.statusCode == 200){
-                let ret = JSON.parse(body);
+
+                let ret = body;
                 if(ret.s > 10000){
                     // error 
                     callback(ret.e,null);
--- test_dotengine.js
+++ test_dotengine.js
@@ -8,5 +8,12 @@ const DotEngine = require('./dotengine');
 let dotEngine = new DotEngine('dotcc','dotcc');
 
 dotEngine.createToken({room:'room',user:'user'},(error,token) => {
-    
+
+    console.log('error ', error);
+    console.log('token ', token);
 });
+
+setTimeout(() => {},3000);
+
+
+
